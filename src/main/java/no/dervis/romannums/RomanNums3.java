6a58c23d2048278e3673568b5032eca6af5376a0
dervism <dervis4@gmail.com>
2020-02-14 01:50:12 +0100

    Refactoring, iteration 13, clean up

--- src/main/java/no/dervis/romannums/RomanNums3.java
+++ src/main/java/no/dervis/romannums/RomanNums3.java
@@ -2,17 +2,17 @@ package no.dervis.romannums;
 
 public class RomanNums3 {
 
-    final String[][] arr = new String[][]{new String[] {"900", "500", "400", "100", "90", "50", "40", "10", "9", "5", "4", "1"},
-            new String[] {"CM", "D", "CD", "C", "XC", "L", "XL", "X" ,"IX", "V", "IV", "I"}};
+    String[] arr = new String[] {"900", "500", "400", "100", "90", "50", "40", "10", "9", "5", "4", "1"};
+    String[] str = new String[] {"CM", "D", "CD", "C", "XC", "L", "XL", "X" ,"IX", "V", "IV", "I"};
 
     public String toRoman(int number) {
         StringBuilder romanText = new StringBuilder();
 
         while (number > 0) {
             int index = 0, c;
-            while ((c = Integer.parseInt(arr[0][index])) > number) index++;
+            while ((c = Integer.parseInt(arr[index])) > number) index++;
             while(number >= c) {
-                romanText.append(arr[1][index]);
+                romanText.append(str[index]);
                 number -= c;
             }
         }
